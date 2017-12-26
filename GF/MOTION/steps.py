1a688156c21fd3064e243aa0b3a636b6ad1921ec
Scott Wiederhold <scott.wiederhold@logicorps.com>
2017-12-26 18:30:23 -0500

    Corrected step decoding comments. Removed unused function.

--- GF/MOTION/steps.py
+++ GF/MOTION/steps.py
@@ -20,12 +20,12 @@ TODO: Error checking/handling of some kind
 """
 _decode_step_codes = {
     'LE': {'mask': 0b00010000, 'test': 0b00010000},  # Laser Enable (ON)
-    'XP': {'mask': 0b00000011, 'test': 0b00000001},  # X-Step
-    'XN': {'mask': 0b00000011, 'test': 0b00000011},  # X-Dir
-    'YP': {'mask': 0b00001100, 'test': 0b00000100},  # Y-Step
-    'YN': {'mask': 0b00001100, 'test': 0b00001100},  # Y-Dir
-    'ZP': {'mask': 0b01100000, 'test': 0b00100000},  # Z-Step
-    'ZN': {'mask': 0b01100000, 'test': 0b01100000},  # Z-Dir
+    'XP': {'mask': 0b00000011, 'test': 0b00000001},  # X+ Step
+    'XN': {'mask': 0b00000011, 'test': 0b00000011},  # X- Step
+    'YP': {'mask': 0b00001100, 'test': 0b00000100},  # Y+ Step
+    'YN': {'mask': 0b00001100, 'test': 0b00001100},  # Y- Step
+    'ZP': {'mask': 0b01100000, 'test': 0b00100000},  # Z+ Step
+    'ZN': {'mask': 0b01100000, 'test': 0b01100000},  # Z- Step
 }
 
 
@@ -42,21 +42,3 @@ def decode_step(step):
                 False if (step & _decode_step_codes[action]['mask']) ^ _decode_step_codes[action]['test'] else True
 
     return step_actions
-
-
-def step_counts(raw_steps):
-    step_count = 0
-    last_step = ''
-    x_count = 0
-    for step in motion_file.raw_pulse:
-        step_count += 1
-        step_actions = ''
-        d_step = steps.decode_step(ord(step))
-
-        for action in d_step:
-            if d_step[action]:
-                step_actions += ' %s' % action
-        if step_actions != '' and step_actions != ' LE':
-            print '%010d: %s' % (step_count, step_actions)
-        if step_actions == ' X+ LE':
-            x_count += 1
