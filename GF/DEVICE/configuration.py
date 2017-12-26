6148d3dff8d9935093a2a180b371b96bfe58c345
Scott Wiederhold <scott.wiederhold@logicorps.com>
2017-12-26 18:10:05 -0500

    Updated project name.

--- GF/DEVICE/configuration.py
+++ GF/DEVICE/configuration.py
@@ -1,6 +1,6 @@
 """
 Copyright 2017 Scott Wiederhold
-This file is part of GF-Library.
+This file is part of Glowforge-Utilities.
 
     GF-Library is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
