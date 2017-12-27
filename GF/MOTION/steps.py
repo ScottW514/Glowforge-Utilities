9571aa947ce46f64b5859c45e674155151717bb3
Scott Wiederhold <scott.wiederhold@logicorps.com>
2017-12-27 13:49:38 -0500

    Fixed to accept binary char, instead of needing ord(bin-char)

--- GF/MOTION/steps.py
+++ GF/MOTION/steps.py
@@ -20,6 +20,7 @@ TODO: Error checking/handling of some kind
 """
 _decode_step_codes = {
     'LE': {'mask': 0b00010000, 'test': 0b00010000},  # Laser Enable (ON)
+    'LP': {'mask': 0b10000000, 'test': 0b01111111},  # Laser Power Setting
     'XP': {'mask': 0b00000011, 'test': 0b00000001},  # X+ Step
     'XN': {'mask': 0b00000011, 'test': 0b00000011},  # X- Step
     'YP': {'mask': 0b00001100, 'test': 0b00000100},  # Y+ Step
@@ -29,15 +30,22 @@ _decode_step_codes = {
 }
 
 
-def decode_step(step):
+def decode_step(raw_step):
+    """
+    Decodes raw steps
+    :param raw_step: {str} Binary representation of step
+    :return: {dict} Decoded step dictionary in format of _decode_step_codes, or LP
+    """
     global _decode_step_codes
+    step = ord(raw_step)
     step_actions = {}
-    if step & 0b10000000:
+    if step & _decode_step_codes['LP']['mask']:
         # Power Setting (LP)
-        step_actions['LP'] = step & 0b01111111
-    else:
-        for action in sorted(_decode_step_codes):
-            # Non Power Setting - Check for actions
+        step_actions['LP'] = step & _decode_step_codes['LP']['test']
+        step = 0
+    for action in sorted(_decode_step_codes):
+        if action != 'LP':
+            # Check for Non-Power actions
             step_actions[action] = \
                 False if (step & _decode_step_codes[action]['mask']) ^ _decode_step_codes[action]['test'] else True
 
